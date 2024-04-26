8abd5df4c5d502b879f96a6c67b80f9e1563e1a8
chittibomma karthik sai <136438998+Karthik-1655@users.noreply.github.com>
2024-04-26 15:48:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const loginBtn = document.querySelector('.login-btn');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
+
+loginBtn.addEventListener('click', () => {
+  wrapper.classList.add('active-login-btn');
+});
+
+iconClose.addEventListener('click', () => {
+  wrapper.classList.remove('active-login-btn');
+});
